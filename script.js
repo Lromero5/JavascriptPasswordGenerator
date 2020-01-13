3af874176123fe596b5bac495e11d9af95007182
LRomero <ldnavar5@gmail.com>
2020-01-12 22:44:45 -0600

    added comments to javascript file

--- script.js
+++ script.js
@@ -1,37 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var allChoices = "";
+
+//This will prompt the user and ask how long do they want their password.
 var howlong = prompt("How long do you want the password?");
 console.log(howlong);
 
+//This will confirm if the user wants uppercase letters
 var wantupper = confirm("Do you want Uppercase letters?");
 console.log(wantupper);
 if (wantupper === true){
   allChoices += "ABCDEFGHIJKLMNOPQRSTUWXYZ";
 }
 
+//this will confirm if the user wants lower case letters
 var wantlower = confirm("Do you want lowercase letters?");
 console.log(wantlower);
 if (wantlower === true){
   allChoices += "abcdefghijklmnopqrstuvwxyz";
 }
 
+//this will confirm if the user wants numbers
 var wantnum = confirm("Do you want numbers?");
 console.log(wantnum);
 if (wantnum === true){
   allChoices += "0123456789";
 }
 
+//this will confirm if the user wants special characters 
 var wantcharacter = confirm("Do you want special characters?");
 console.log(wantcharacter);
 if (wantcharacter === true){
   allChoices += "!@#$%^&*()";
 }
 
-// Write password to the #password input
+// this function will write password to the #password input
 function writePassword() {
+  //this will show in the console log when the generate password button is clicked
   console.log("You got clicked");
-  // var allChoices = "abcdefghijklmnopqrstuvwxyz";
 
   var password = ""; //This will collect info from the for loop
 
@@ -50,4 +56,3 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// BONUS EVENT LISTENER
